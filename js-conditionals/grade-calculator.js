49ca772c4e700f4f0cb07d520dc39e857d25b34f
Shuprov <asifurshuprov@gmail.com>
2025-07-30 22:43:17 +0600

    grade calculator added

---
+++ js-conditionals/grade-calculator.js
@@ -0,0 +1,16 @@
+const studentMarks = -70;
+if (studentMarks >= 90 && studentMarks <= 100) {
+    console.log("Your grade is: A");
+}
+else if (studentMarks >= 80 && studentMarks <= 89) {
+    console.log("Your grade is: B");
+}
+else if (studentMarks >= 70 && studentMarks <= 79) {
+    console.log("Your grade is: C");
+}
+else if (studentMarks >= 60 && studentMarks <= 69) {
+    console.log("Your grade is: D");
+}
+else if (studentMarks >= 0 && studentMarks <= 59) {
+    console.log("Your grade is: F");
+}
