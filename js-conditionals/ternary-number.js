cde66f9ea9c276e1458ac41f87834778c24f1cda
Shuprov <asifurshuprov@gmail.com>
2025-07-31 00:26:16 +0600

    ternary number done

---
+++ js-conditionals/ternary-number.js
@@ -0,0 +1,17 @@
+//simple if-else approach
+const num1 = 75;
+const num2 = 70;
+let result;
+// if (num1 > num2) {
+//     result = num1 * 2;
+//     console.log(result);
+// }
+// else {
+//     result = num1 + num2;
+//     console.log(result);
+// }
+
+
+//ternary operator approach
+result = num1 > num2 ? num1 * 2 : num1 + num2;
+console.log(result);
