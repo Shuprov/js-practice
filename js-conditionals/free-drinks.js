3f0a5fbd459bf63d318d7d2cb70ccb26cd51afa9
Shuprov <asifurshuprov@gmail.com>
2025-07-30 20:41:50 +0600

    free-drinks problem solved

---
+++ js-conditionals/free-drinks.js
@@ -0,0 +1,8 @@
+const burgerPrice = 400;
+const cokePrice = 30;
+if (burgerPrice > 500) {
+    console.log("You won a free coke");
+}
+else {
+    console.log("You have to pay " + cokePrice + " taka for coke");
+}
