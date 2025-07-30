4fd88beb751573e0475eb661579c437928b644b5
Shuprov <asifurshuprov@gmail.com>
2025-07-30 21:24:13 +0600

    BMI calculator done

---
+++ js-conditionals/BMI-calculator.js
@@ -0,0 +1,15 @@
+const weight = 45;
+const height = 1.5748;
+const BMI = weight / (height) ** 2;
+if (BMI < 18.5) {
+    console.log("you are underweight.");
+}
+else if (BMI >= 18.5 && BMI <= 24.9) {
+    console.log("you are normal.");
+}
+else if (BMI >= 25 && BMI <= 29.9) {
+    console.log("you are overweight.");
+}
+else {
+    console.log("you are obese.");
+}
