7561b0648a94a4edf6a4a8685fe22edab311a5fe
Shuprov <asifurshuprov@gmail.com>
2025-07-31 01:49:01 +0600

    isNaN updated

--- js-variables/isNaN.js
+++ js-variables/isNaN.js
@@ -1,3 +1,6 @@
+// The isNaN() function in JavaScript is used to check whether a value is "Not-a-Number" (NaN). It returns true if the value is NaN or cannot be converted to a number, and false if it’s a valid number or can be coerced into one.
+
+
 var a = isNaN('11');
 console.log(a); //returns false
 
